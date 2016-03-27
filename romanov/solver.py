172e462273f7e03fd3ea0b363e8ec2dc662c5964
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-03-27 18:35:52 +0200

    solver: add Solver class

---
+++ romanov/solver.py
@@ -0,0 +1,18 @@
+import romanov.core
+
+class Solver:
+    def __init__(self, logic = 'QF_AUFBV', has_model = True, options = {}):
+        self._logic = logic
+        self.has_model = has_model
+        self._options = options
+
+    def reset(self):
+        for key, val in self._options:
+            self.emit('(set-option :{} {}\n)'.format(key, val))
+        self.emit('(set-logic {})\n'.format(self._logic))
+        
+    def emit(self, smt):
+        raise NotImplementedError
+
+    def recv(self):
+        raise NotImplementedError
