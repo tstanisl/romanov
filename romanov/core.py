b846556cc6657997e8abf54b404e43b38d5c5598
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-04-01 22:35:09 +0200

    core: add current(), make_current()

--- romanov/core.py
+++ romanov/core.py
@@ -9,3 +9,16 @@ class Context:
     "Public wrapper class for _Context"
     def __init__(self):
         self._priv = _Context()
+
+def current():
+    "Returns active context. May return None."
+    return getattr(current, "context", None)
+
+def make_current(new):
+    "Sets new active context. Returns previous one."
+    if not(isinstance(new, Context) or new is None):
+        raise TypeError('Argument of make_current() must be Context or None')
+
+    old = current()
+    current.context = new
+    return old
