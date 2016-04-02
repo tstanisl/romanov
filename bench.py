6a5ba253dd6a622e493451d05c8590fdd9c78b8c
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-04-02 11:32:51 +0200

    bench: simple engine for speed comparison

---
+++ bench.py
@@ -0,0 +1,35 @@
+from subprocess import Popen, PIPE, TimeoutExpired
+from time import time
+import sys
+
+players = {
+    'stp' : ('stp',),
+    'cvc4' : ('cvc4',),
+    'btor' : ('boolector',),
+    'btor2' : ('boolector2',),
+    'z3' : ('z3',),
+    'msat' : ('mathsat','-config=smtcomp2015_main.txt'),
+    'yices' : ('yices-smt2',),
+}
+
+for arg in sys.argv[1:]:
+    for nick, cmd in players.items():
+        cmdline = cmd + (arg,)
+        tic = time()
+        pipe = Popen(cmdline, stdout=PIPE)
+        try:
+            ans, _ = pipe.communicate(timeout=30)
+            ans = ans.decode().strip()
+            pipe = None
+        except TimeoutExpired:
+            ans = 'timeout'
+        except:
+            ans = 'failure'
+
+        if pipe:
+            pipe.kill()
+            pipe.communicate()
+
+        toc = time()
+        print(arg, nick, ans, str(toc - tic))
+
