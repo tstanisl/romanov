5bb1dfc29828aebb674585c7fef0afb6ee43c3e3
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-04-03 18:05:08 +0200

    op2: fixes

--- op2.py
+++ op2.py
@@ -18,7 +18,7 @@ bits = 32
 itype = '(_ BitVec 32)'
 etype = '(_ BitVec {})'.format(bits)
 atype = '(Array {} {})'.format(itype, etype)
-rnd = lambda: '#x{:08x}'.format(random.randrange(R))
+rnd = random.randrange
 ops = ['bvadd', 'bvsub', 'bvor', 'bvnor', 'bvand', 'bvnand', 'bvxor']
 
 print('; Seed = {}'.format(Seed))
@@ -28,17 +28,41 @@ if mode == 'def':
     for r in range(R):
         print('(declare-fun X{} () {})'.format(r, etype))
     for k in range(K):
-        #x = 'X' + str(random.randrange(R + k))
         x = 'X' + str(R + k - 1)
         if k & 1:
-            y = 'X' + str(random.randrange(R + k))
-            #y = 'X' + str((R + k) // 2)
+            y = 'X' + str(rnd(R + k))
         else:
-            y = '#x{:0{digits}x}'.format(random.randrange(2**bits), digits=bits//4)
-        op = ops[random.randrange(len(ops))]
+            y = '#x{:0{digits}x}'.format(rnd(2**bits), digits=bits//4)
+        op = ops[rnd(len(ops))]
         print('(define-fun X{} () {} ({} {} {}))'.format(
               R + k, etype, op, x, y))
     print('(assert (= X{} X0))'.format(R + K - 1))
+elif mode == 'dec':
+    for r in range(R + K):
+        print('(declare-fun X{} () {})'.format(r, etype))
+    for k in range(K):
+        x = 'X' + str(R + k - 1)
+        if k & 1:
+            y = 'X' + str(rnd(R + k))
+        else:
+            y = '#x{:0{digits}x}'.format(rnd(2**bits), digits=bits//4)
+        op = ops[rnd(len(ops))]
+        print('(assert (= X{} ({} {} {})))'.format(R + k, op, x, y))
+    print('(assert (= X{} X0))'.format(R + K - 1))
+elif mode == 'let':
+    for r in range(R):
+        print('(declare-fun X{} () {})'.format(r, etype))
+    print('(assert')
+    for k in range(K):
+        x = 'X' + str(R + k - 1)
+        if k & 1:
+            y = 'X' + str(rnd(R + k))
+        else:
+            y = '#x{:0{digits}x}'.format(rnd(2**bits), digits=bits//4)
+        op = ops[rnd(len(ops))]
+        print('(let ((X{} ({} {} {})))'.format(R + k, op, x, y))
+    print('(= X{} X0)'.format(R + K - 1))
+    print(')' * (K + 1))
 else:
     assert False, "Invalid mode"
 
