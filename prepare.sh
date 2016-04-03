e69e3981756a43634606536a318a5fdf2ccbb438
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-04-03 18:05:42 +0200

    prepare: use loops

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,9 @@
-python3 writeb.py let 1024 64 1 > writeb_1024_64_let_sat.smt2
-python3 writeb.py def1 1024 64 1 > writeb_1024_64_def1_sat.smt2
-python3 writeb.py def2 1024 64 1 > writeb_1024_64_def2_sat.smt2
-python3 writeb.py let 1024 64 2 > writeb_1024_64_let_unsat.smt2
-python3 writeb.py def1 1024 64 2 > writeb_1024_64_def1_unsat.smt2
-python3 writeb.py def2 1024 64 2 > writeb_1024_64_def2_unsat.smt2
+for mode in let def1 def2 declet declet2; do
+	python3 writeb.py $mode 1024 64 1 > writeb_1024_64_"$mode"_sat.smt2
+	python3 writeb.py $mode 1024 64 2 > writeb_1024_64_"$mode"_unsat.smt2
+done
+
+for mode in let def dec; do
+	python3 op2.py $mode 2048 4 1 > op2_"$mode"_sat.smt2
+	python3 op2.py $mode 2048 4 8 > op2_"$mode"_unsat.smt2
+done
