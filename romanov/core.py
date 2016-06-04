0e58e17da89d592beab24962c6476deb352c2b9e
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-06-04 20:21:36 +0200

    core: add Opcode class

--- romanov/core.py
+++ romanov/core.py
@@ -10,3 +10,16 @@ class Encodable(ABC):
     @abstractmethod
     def encode(self, encoder):
         "Encodes objects in SMTLIB2"
+
+class Opcode(Encodable):
+    "Abstraction of result of SMTLIB2 operation"
+    def __init__(self, smt2op, returns, *args):
+        super().__init__()
+        self.smt2op = smt2op
+        self.returns = returns
+        self.args = args
+
+    def encode(self, encoder):
+        args = [encoder.encode(arg) for arg in self.args]
+        formula = '({} {})'.format(self.smt2op, ' '.join(args))
+        return formula
