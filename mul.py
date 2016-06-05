b56a6026344cc00ab5d08ca1e1e7eca0ebc6add5
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-06-05 09:14:49 +0200

    mul: redesign extend mode

--- mul.py
+++ mul.py
@@ -9,7 +9,7 @@ try:
 
     assert obits >= 2 * ibits
 
-    seed = argv[4] if len(sys.argv) >= 5 else time.time()
+    seed = sys.argv[4] if len(sys.argv) >= 5 else time.time()
     seed = int(seed)
 except:
     print("Unexpected error:", sys.exc_info()[1])
@@ -22,12 +22,30 @@ rnd = random.randrange
 R = rnd(1 << (2 * ibits))
 
 print('; seed = ', seed)
+print('(set-logic QF_AUFBV)')
+
 if mode == 'bounds':
     print('(declare-fun A () (_ BitVec {}))'.format(obits))
     print('(declare-fun B () (_ BitVec {}))'.format(obits))
     print('(assert (bvult A (_ bv{} {})))'.format(1 << ibits, obits))
     print('(assert (bvult B (_ bv{} {})))'.format(1 << ibits, obits))
     print('(assert (= (bvmul A B) (_ bv{} {})))'.format(R, obits))
+#elif mode == 'extend':
+#    print('(declare-fun A () (_ BitVec {}))'.format(ibits))
+#    print('(declare-fun B () (_ BitVec {}))'.format(ibits))
+#    print('(assert')
+#    print('(let ((a ((_ sign_extend {}) A)))'.format(obits - ibits))
+#    print('(let ((b ((_ sign_extend {}) B)))'.format(obits - ibits))
+#    print('(= (bvmul a b) (_ bv{} {}))'.format(R, obits))
+#    print(')))')
+elif mode == 'extend':
+    print('(declare-fun A () (_ BitVec {}))'.format(ibits))
+    print('(declare-fun B () (_ BitVec {}))'.format(ibits))
+    print('(assert')
+    print('(let ((a ((_ zero_extend {}) A)))'.format(obits - ibits))
+    print('(let ((b ((_ zero_extend {}) B)))'.format(obits - ibits))
+    print('(= (bvmul a b) (_ bv{} {}))'.format(R, obits))
+    print(')))')
 else:
     assert False, "Invalid mode"
 
