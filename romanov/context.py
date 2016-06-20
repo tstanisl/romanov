63ae213145936f481ea789458507363afb873fcb
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-06-20 18:48:03 +0200

    context: simplifications

--- romanov/context.py
+++ romanov/context.py
@@ -1,14 +1,9 @@
 "Core classes and functions of Romanov system"
 
-class _Context:
-    "Private class used to trace assumes, asserts and dumps."
+class Context:
+    "Class used to trace assumes, asserts and dumps."
     def __init__(self):
         pass
-
-class Context:
-    "Public wrapper class for _Context"
-    def __init__(self):
-        self._priv = _Context()
 
 def current():
     "Returns active context. May return None."
@@ -16,11 +11,9 @@ def current():
 
 def make_current(new):
     "Sets new active context. Returns previous one."
-    if not(isinstance(new, Context) or new is None):
+    if not isinstance(new, (type(None), Context)):
         raise TypeError('Argument of make_current() must be Context or None')
 
     old = current()
     current.context = new
     return old
-
-
