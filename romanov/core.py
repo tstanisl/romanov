5c32787b86fad95d6e78b2d246f9b89b2a4edc0b
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-06-24 16:37:06 +0200

    core: add Memory class

--- romanov/core.py
+++ romanov/core.py
@@ -219,3 +219,21 @@ def bitvec(bits, value=None):
        Shortcut for BitVecClass(bits)(value)."""
     cls = BitVecClass(bits)
     return cls(value)
+
+class Memory(Symbolic):
+    "Abstraction of memory state."
+    def __init__(self, value):
+        super().__init__(value)
+
+    @abstractmethod
+    def read(self, addr):
+        "Reads one byte (BitVec8) from Memory."
+
+    @abstractmethod
+    def write(self, addr, data):
+        "Returns new Memory after storing byte {data} at address {addr}/."
+
+    @abstractmethod
+    def copy(self, dst, src, size):
+        """Implements C-style memmove({dst}, {src}, {size}).
+           Return Memory object after operation"""
